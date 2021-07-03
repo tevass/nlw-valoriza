3f4fcd6a02afad5782797f55b3f06df25019f65d
Estevão Gabriel <teh.gabriel10@gmail.com>
2021-07-02 23:32:43 -0300

    finished class two

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
+import 'reflect-metadata'
 import express from 'express'
+
+import { router } from './routes'
+
+import './database'
 
 const app = express()
 
-app.get('/test', (req, res) => {
-
-    return res.send('/test')
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(3000, () => {
     console.log('Server is running')
